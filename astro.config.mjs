344c0f11e2bec88c7c02bac14613691d2c8a8b1b
guoenran <3587180458@qq.com>
2025-11-18 08:52:54 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site:"https://emjyg.netlify.app/"
+  site:"https://emjyg.netlify.app/",
+  integrations: [preact()]
 });
